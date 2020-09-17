a90dc68de3bf9082c5d67d31d7bb41ac122ff821
alan <alan@newverveconsulting.com>
2020-09-17 14:09:17 +0100

    adding form input for sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -11,8 +12,9 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Switch>
-          <Route exact path={'/'} component={HomePage} />
-          <Route exact path={'/shop'} component={ShopPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
